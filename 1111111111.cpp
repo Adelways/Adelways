bb5285e909e0823e28211fa3ba513cb0cf4d2e1b
Adelways <78844804+Adelways@users.noreply.github.com>
2021-02-18 13:37:52 +0300

    Add files via upload

---
+++ 1111111111.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+	//setlocale(LC_ALL, "rus");
+	int n,a,b,c;
+	scanf("%d", &n);
+	a = n / 100;
+	b = n / 10 % 10;
+	c = n % 10;
+	//print ("ясллю: %d\n", a+b+c);
+	//print ("опнхгбедемхе: %d\n", a*b*c);
+}
